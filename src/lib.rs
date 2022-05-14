874ab3d3863c4fc0b489022e42d9b654c50692ca
Nishant-Pall <nishant@loopinhq.com>
2022-05-14 12:17:00 +0530

    Use keyword

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+fn serve() {}
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+
+    fn fix_order() {
+        // super keyword
+        super::serve();
+    }
+}
+// use keyword
+// either absolute or relative
+// use crate::front_of_house::hosting;
+use self::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat")
 }
